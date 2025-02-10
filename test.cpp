02967fa8bfc385ed5b868028cbeaad5ba086551f
ghanap <anagha.pillalamarri@gmail.com>
2025-02-10 09:09:46 +0530

    Add test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,6 @@
+// Your First C++ Program
 #include <iostream>
-using namespace std;
-
 int main() {
-    int n, i;
-
-    cout << "Enter a positive integer: ";
-    cin >> n;
-
-    cout << "Factors of " << n << " are: ";  //print statement
-    for(i = 1; i <= n; ++i) {
-        if(n % i == 0)
-            cout << i << " ";
-    }
-
+    std::cout << "Hello World!";
     return 0;
 }
